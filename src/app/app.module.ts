6e2588bcb4ec2e8e6964412b62dc9141c988df53
luizfilipe <luizkato8@Gmail.com>
2023-11-12 23:34:15 -0400

    fix: organazing modules and app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModele } from './app-routing.module';
 import { HomeModule } from './components/home/home.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from "@angular/core";
-import { NgModel } from "@angular/forms";
 import { HomeComponent } from "./home.component";
 import { HeaderComponent } from "../../shared/header/header.component";
 import { BtnPrimaryComponent } from "../../shared/btn-primary/btn-primary.component";
 import { BrowserModule } from "@angular/platform-browser";
+import { MainSectionComponent } from "src/app/main-section/main-section/main-section.component";
+import { MainAnimationComponent } from "src/app/main-animation/main-animation/main-animation.component";
 
 @NgModule({
     declarations: [
         HomeComponent,
         HeaderComponent,
-        BtnPrimaryComponent
+        BtnPrimaryComponent,
+        MainSectionComponent,
+        MainAnimationComponent
     ],
     imports: [
         BrowserModule,
@@ -18,6 +21,6 @@ import { BrowserModule } from "@angular/platform-browser";
     bootstrap: [],
     exports: [
         HomeComponent
-    ]
+    ],
 })
 export class HomeModule {}
