0962fcee3ec50b43da8b5a66ccb8582f5336d973
luizfilipe <luizkato8@Gmail.com>
2023-11-23 23:40:33 -0300

    fix: add section-projects and redes-sociais section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModele } from './app-routing.module';
 import { HomeModule } from './components/home/home.module';
 import { HomeComponent } from './components/home/home.component';
+import { AboutSectionComponent } from './about/about-section/about-section.component';
+import { LineAnimationComponent } from './about/line-animation/line-animation.component';
+import { ProjectsSectionComponent } from './projects-section/projects-section/projects-section.component';
+import { RedesSociaisSectionComponent } from './redes-socias-section/redes-sociais-section/redes-sociais-section.component';
+import { RedesSociaisAnimationComponent } from './redes-sociais-section/redes-sociais-animation/redes-sociais-animation.component';
+import { FotterComponent } from './footer/fotter/fotter.component';
 
 @NgModule({
   declarations: [
